4f298fa952c7c8cffd0b36934475647770fde201
Max Kremmel <xing@synapse.plus.com>
2006-01-17 20:13:51 +0000

    use PKG_NAME for app menu key and dir name for menu title - needed for icons on 'my' page

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -2,8 +2,8 @@
 global $gBitSystem;
 $gBitSystem->registerPackage('xmlrpc', dirname(__FILE__).'/' );
 
-	if($gBitSystem->isPackageActive( 'xmlrpc' ) ) {
+//	if($gBitSystem->isPackageActive( 'xmlrpc' ) ) {
 //		$gBitSystem->registerAppMenu( XMLRPC_PKG_DIR, 'XMLRPC', XMLRPC_PKG_URL.'index.php', 'bitpackage:xmlrpc/menu_xmlrpc.tpl', 'xmlrpc');
-	}
+//	}
 
 ?>
