a5a4cbe17411aaeb5d8cca8b5c3233ea7f603239
Max Kremmel <xing@synapse.plus.com>
2006-02-17 23:51:56 +0000

    registerPackage now takes a hash instead of multiple parameters - keeps # of parameters under control

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -1,9 +1,14 @@
 <?php
 global $gBitSystem;
-$gBitSystem->registerPackage('xmlrpc', dirname(__FILE__).'/' );
 
-//	if($gBitSystem->isPackageActive( 'xmlrpc' ) ) {
-//		$gBitSystem->registerAppMenu( XMLRPC_PKG_DIR, 'XMLRPC', XMLRPC_PKG_URL.'index.php', 'bitpackage:xmlrpc/menu_xmlrpc.tpl', 'xmlrpc');
-//	}
+$registerHash = array(
+	'package_name' => 'xmlrpc',
+	'package_path' => dirname( __FILE__ ).'/',
+);
+$gBitSystem->registerPackage( $registerHash );
+
+//if($gBitSystem->isPackageActive( 'xmlrpc' ) ) {
+//	$gBitSystem->registerAppMenu( XMLRPC_PKG_DIR, 'XMLRPC', XMLRPC_PKG_URL.'index.php', 'bitpackage:xmlrpc/menu_xmlrpc.tpl', 'xmlrpc');
+//}
 
 ?>
